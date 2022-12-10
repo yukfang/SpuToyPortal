d07e1f82acb46bace78e419e571eb4d5ad222288
方宇 <fangyu.k@bytedance.com>
2022-12-10 22:43:21 +0800

    fun

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const {koaApp, init} = require('./koaApp');
 const PORT = process.env.PORT || 80
 
-if(process.env.PLATFORM != 'FAAS') { // FaaS does not require consul service
-    init();
-    koaApp.listen(PORT);
-} else {
+if(process.env.PLATFORM == 'FAAS') { // Set this ENV in FAAS to make it work
     exports.handler = koaApp.callback();
     exports.initializer = init;
+} else { // Local Test
+    init();
+    koaApp.listen(PORT);
 }
